edf12c9c1c6ac28df6f5fd8270455cf7a389bc6e
Lee HanYeong <dev@azelf.com>
2016-11-10 17:02:53 +0900

    member, photo app추가, AUTH_USER_MODEL지정, migrations생성

--- django_app/instagram/settings.py
+++ django_app/instagram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'member',
+    'photo',
 ]
 
 MIDDLEWARE = [
@@ -80,10 +83,9 @@ DATABASES = {
     }
 }
 
+# Auth
 
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
